09e00b3f801d7cf35b508195426e1c990a7baa36
rs691 <robstewart@my365.bellevue.edu>
2025-01-23 00:02:01 -0600

    All changes are committed

--- Mod4/mod4.php
+++ Mod4/mod4.php
@@ -1,11 +1,12 @@
 <?php 
-
 $name = "John";
 echo "Hello, {$name} Smith!"; // Output: Hello, JohnDoe!
 
+//as we know, positions are 0,1,2
 $colors = ['red', 'blue', 'green'];
-echo "My favorite color is {$colors[1]}."; // Output: My favorite color is blue.
+// Output: My favorite color is blue.
+echo "My favorite color is {$colors[1]}."; 
 
+// Output: The area of a square with side 5 is 25.
 $length = 5;
 echo "The area of a square with side {$length} is " . ($length ** 2) . "."; 
-// Output: The area of a square with side 5 is 25.
